df2d84bfc093549b8c44d392916d961d4869f7ec
AndreaLolli2912 <andrea.lolli1@outlook.com>
2025-06-30 18:57:10 +0200

    Update calibration batch size to 100 and modify model weight filename format

--- NLQ/main_quantization.py
+++ NLQ/main_quantization.py
@@ -104,7 +104,7 @@ def main(configs, parser):
     model = apply_post_training_static_quantization(
         float_model=model,
         calibration_loader=train_loader,
-        num_calibration_batches=500
+        num_calibration_batches=100
     )
 
     score_writer = open(
@@ -141,7 +141,7 @@ def main(configs, parser):
         model.state_dict(),
         os.path.join(
             model_dir,
-            "{}_{}.t7".format(configs.model_name, 0),
+            f"{configs.model_name}_quantized_weights.t7",
         ),
     )
     model.train()
